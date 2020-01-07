36a352cdc1ae284b21693d9a245a085c8d498406
Mentosento <50349325+Mentosento@users.noreply.github.com>
2020-01-07 14:24:51 -0500

    Test space, basic movement, script folder, material folder, test scene, temp character.
    Made a test scene with a testing space within it. Added basic left and right movement with the A & D keys, as well as the Left and Right arrow keys. These can be removed in future iterations. Script folder has been added, this folder will be reserved for MAJOR SCRIPTS ONLY, any small or personal scripts should be done in a personal script folder to prevent conflicts in later commits. Added material folder for organization, as well as a temp character material. Added a temp character model, Empty Game Object called Character parents a Cube with a 2D Box Collider. Character script is on the cube, as well as a Rigidbody2D.

---
+++ ThatOnePlace2D/Assets/Scripts/Character.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    //Variables
+    Rigidbody2D rb; //Setting the character's Rigidbody component to rb, can call with rb.etc
+    bool bleftReq, bRightReq; //Setting a bool for movement with GetKey
+    float fSpeed = 20f; //Character movement speed
+    Vector2 vDirModifier; //Character Vector2
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.mass = 300; //Hardcode for character mass, adjust through this variable only, not through inspector.
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) //Can remove 2nd Input if we don't want arrow key control.
+        {
+            bleftReq = true;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) // Cam remove 2nd Input if we don't want arrow key control.
+        {
+            bRightReq = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        vDirModifier.x = 0;
+
+        if (bleftReq)
+        {
+            bleftReq = false;
+            vDirModifier.x -= fSpeed;
+        }
+
+        if (bRightReq)
+        {
+            bRightReq = false;
+            vDirModifier.x += fSpeed;
+        }
+        rb.velocity = vDirModifier;
+    }
+
+}
