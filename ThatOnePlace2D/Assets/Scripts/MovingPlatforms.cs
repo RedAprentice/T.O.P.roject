8e4e46b7e0c8015b3e879f2c2e5ccf002a5ce0f3
Frank Fontana <catmo8@cox.net>
2020-02-12 16:15:40 -0600

    Working on MovingPlatform script

---
+++ ThatOnePlace2D/Assets/Scripts/MovingPlatforms.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour
+{
+    public Vector3 velocity;
+    public float platformSpeed;
+
+    bool moving;
+
+    private void OnCollisionEnter(Collision2D collision)
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += (velocity * Time.deltaTime * platformSpeed);
+    }
+}
