3ad6d6351153f9c40c9ff43e823c44afe8bd5fcf
Steven Huynh <steven.h.mainacc@gmail.com>
2020-03-14 06:16:10 -0500

    Partial Progress with Grappling Hook
    Not working.
    Did some code cleaning and partial writing and structuring.
    Bugs found with Low Reproducibility
      - Player can sometimes stick to wall
      - Player can jump again before touching ground. Likely because of the wall

--- ThatOnePlace2D/Assets/Scripts/StevenScripts/playerMovement.cs
+++ ThatOnePlace2D/Assets/Scripts/StevenScripts/playerMovement.cs
@@ -4,54 +4,75 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    // Variables
+    // General Movement Variables
     [SerializeField] private Rigidbody2D player; // Player Object
     [SerializeField] private BoxCollider2D boxCollider2D; // Player Collider
     [SerializeField] private float movementFactor = 5; // movement speed modifier
     [SerializeField] private float jumpFactor = 30; // jump height modifier
-    [SerializeField] private float bufferSpace = 0.5f; // extra space above the player can jump
     [SerializeField] private float gravity = 10;
     [SerializeField] private string xControl = "Horizontal";
     [SerializeField] private string yControl = "Vertical";
-    [SerializeField] private LayerMask m_GroundLayers;
-    private float xInput;
-    private float yInput;
-    private Vector2 pVelocity;
-    private Vector2 curVelocity;
-    private bool isGrounded;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private Transform ceilingCheck;
-    const float groundRadius = 0.05f;
-    
-    
+
+    // Ground Check Variables
+    private bool isGrounded; // true if touching ground
+    [SerializeField] private LayerMask m_GroundLayers; // Layers isGrounded interacts with
+    [SerializeField] private float bufferSpace = 0.5f; // extra space below the player can jump
+
+    // Grappling Hook Variables
+    [SerializeField] private GameObject gHook;
+    [SerializeField] private float hookSpeedFactor = 1f;
+    [SerializeField] private float hookPullForceFactor = 1f;
+    private Vector2 hookDirection;
+    private bool hookOut;
+    private bool hookGrappled;
+
+    // Movement Buffering Variables
+    [SerializeField] private string hookButton = "Fire1";
+    private bool bufHook;
+    [SerializeField] private string dodgeButton = "Fire2";
+    private bool bufDodge;
+    [SerializeField] private string otherThing;
+    private bool bufOther;
+
     // Start is called before the first frame update
     void Start()
     {
         player.gravityScale = gravity;
-
+        hookOut = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        BasicMovement();
 
+        // NOTE FOR THE FUTURE
+        // Implement buffers so that movement can be restricted during abilities
 
+        basicMovement();
+
+        if ( bufHook )
+        {
+            bufHook = false;
+            shootGrapplingHook();
+        }
+
+        gHookMovement();
 
     }
 
     private void FixedUpdate()
     {
-        CharacterChecks(); // idk if this should be here. As of here for cleaniness.
+        characterChecks(); // idk if this should be here. As of here for cleaniness.
     }
 
-    private void BasicMovement()
+    private void basicMovement()
     {
 
-        curVelocity = player.velocity;
+        Vector2 curVelocity = player.velocity;
+        Vector2 pVelocity;
 
-        xInput = Input.GetAxis(xControl); // I don't know if this will work, but hey.
-        yInput = Input.GetAxis(yControl);
+        float xInput = Input.GetAxis(xControl); // I don't know if this will work, but hey.
+        float yInput = Input.GetAxis(yControl);
 
         if (xInput != 0)
         {
@@ -60,7 +81,7 @@ public class playerMovement : MonoBehaviour
             player.velocity = pVelocity;
         }
 
-        if (yInput != 0 && isGrounded == true)
+        if (yInput > 0 && isGrounded == true)
         {
             isGrounded = false;
             pVelocity.y = jumpFactor;
@@ -70,7 +91,7 @@ public class playerMovement : MonoBehaviour
 
     }
 
-    void CharacterChecks()
+    private void characterChecks()
     {
         RaycastHit2D collided = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, bufferSpace, m_GroundLayers);
 
@@ -83,4 +104,67 @@ public class playerMovement : MonoBehaviour
         }
     }
     
+    private void shootGrapplingHook()
+    {
+        if (hookOut == false)
+        {
+            if ( hookGrappled == false )
+            {
+                // activate the hook
+                hookOut = true;
+                // save the direction
+                Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 curPos = transform.position;
+
+                hookDirection = mouse - curPos;
+                hookDirection = hookDirection / hookDirection.magnitude; // Intensive because sqrt. Maybe look for another way later?
+            } else // when hookGrappled is true
+            {
+                // release the grapple
+                grappleRelease();
+            }
+            
+        }
+
+    }
+
+    private void gHookMovement ()
+    {
+        if (hookOut)
+        {
+            hookTravel();
+        }
+
+        if (hookGrappled)
+        {
+            // move the player to the hooked location
+            // NOTE: Fly to the tile grabbed, but the last part of the
+            //       movement might need to be done "strangely". Hopefully not
+
+            transform.position = (Vector2)transform.position + hookDirection*hookSpeedFactor*Time.deltaTime;
+            // or
+            // player.AddForce(hookDirection * hookPullForceFactor);
+        }
+    }
+
+    private void hookTravel()
+    {
+        // move the hook around until it hits something
+
+        if (/* we hit something */ true) // probably going to be moved since it needs onTriggerEnter()
+        {
+            // when it hits
+            hookOut = false;
+            hookGrappled = true;
+            player.gravityScale = 0; // turn off gravity while we are flying
+            // hookDirection = transform.position - /* middle of the tile we are going to snap to*/ ;
+        }
+
+    }
+
+    private void grappleRelease() // might need more steps?
+    {
+        hookGrappled = false;
+        player.gravityScale = gravity;
+    }
 }
